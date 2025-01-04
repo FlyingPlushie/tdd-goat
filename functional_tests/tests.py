bb6072e627f3df0c2128484fa6f7dbf952e4e769
Stefan M. <stefan.mx.mitrovic@gmail.com>
2025-01-05 00:29:05 +0100

    Use os.environ.get instead of os.getenv

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -13,7 +13,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        test_server = os.getenv('TEST_SERVER')
+        test_server = os.environ.get('TEST_SERVER')
         if test_server:
             self.live_server_url = 'http://' + test_server
     
